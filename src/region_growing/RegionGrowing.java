a2d8b133288bf3bb6357027d7d410aaf8749f2ae
João Victor Claudino Felipe <victorskg@outlook.com>
2018-08-07 23:50:57 -0300

    Crescimento de regiões pelo pixel central.

--- src/region_growing/RegionGrowing.java
+++ src/region_growing/RegionGrowing.java
@@ -1,87 +1,63 @@
 package region_growing;
 
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RegionGrowing {
     public void run(String[] args) {
-        String imagemPadrao = "src/images/3310.jpg";
-        int diferencaPadrao = 50; //Altere para um resultado diferente na imagem após o crescimento de regiões
-        List<Regiao> regioes = new ArrayList<>();
+        File diretorio = new File("src/images/MediumCut"); //Carrega o diretorio que contem as imagens
+        String[] arquivos = diretorio.list(); //Cria uma lista com o nome dos arquivos
+        int limiar = 50; //Altere para um resultado diferente na imagem após o crescimento de regiões
+        double[] corRGB = {127, 127, 127};
 
-        //Verifica os argumentos do programa
-        String caminhoImagem = ((args.length > 0) ? args[0] : imagemPadrao);
-        int diferencaCores = ((args.length > 1) ? Integer.parseInt(args[1]) : diferencaPadrao);
+        for(int k = 0; k < arquivos.length; k++) {
+            //Carrega a imagem
+            Mat imagem = Imgcodecs.imread(diretorio.getPath()+"/"+arquivos[k], Imgcodecs.IMREAD_COLOR);
+            //Verifica se a imagem foi carregada com sucesso
+            if( imagem.empty() ) {
+                System.out.println("Erro ao abrir imagem: " + arquivos[k]);
+                System.exit(-1);
+            }
 
-        //Carrega a imagem
-        Mat imagem = Imgcodecs.imread(caminhoImagem, Imgcodecs.IMREAD_COLOR);
+            Regiao regiao = new Regiao();
+            Point seed = new Point(imagem.size().height/2, imagem.size().width/2);
+            int xSeed = (int) seed.x - 10;
+            int ySeed = (int) seed.y + 10;
 
-        //Verifica se a imagem foi carregada com sucesso
-        if( imagem.empty() ) {
-            System.out.println("Erro ao abrir imagem! Por favor, passe como argumento: [caminho para imagem] e [diferenca de cores].");
-            System.exit(-1);
-        }
-
-        HighGui.imshow("Imagem Original", imagem);
-
-        //Binarização da imagem
-        Mat imagem_binaria = new Mat();
-        Imgproc.cvtColor(imagem, imagem_binaria, Imgproc.COLOR_BGR2GRAY);
-        Imgproc.threshold(imagem_binaria, imagem_binaria, 40, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
-        HighGui.imshow("Imagem Binária", imagem_binaria);
-
-        //Crescimento de regiões
-        Mat imagemCrescimentoRegioes = imagem.clone();
-        byte[] srcData = new byte[(int) (imagemCrescimentoRegioes.total() * imagemCrescimentoRegioes.channels())]; //Dados dos pixels da imagem
-        imagemCrescimentoRegioes.get(0, 0, srcData); //Pegando os dados em termos de coordenadas de linhas e colunas
-        for (int i = 0; i < imagemCrescimentoRegioes.rows(); i++) {
-            for (int j = 0; j < imagemCrescimentoRegioes.cols(); j++) {
-                Pixel pixel = new Pixel(i, j, imagemCrescimentoRegioes.get(i, j));
-
-                //Primeira região criada
-                if (regioes.isEmpty()) {
-                    Regiao regiao = new Regiao();
-                    regiao.pixels.add(pixel);
-                    regioes.add(regiao);
-                    System.out.println("Primeira região");
-                } else {
-                    boolean adicionado = false;
-
-                    //Laço que verifica se o novo pixel pertence a uma nova região ou a uma já existente
-                    for (int k = 0; k < regioes.size(); k++) {
-                        if( Math.abs(regioes.get(k).pixels.get(0).intensidade[0] - imagemCrescimentoRegioes.get(i, j)[0]) +
-                                Math.abs(regioes.get(k).pixels.get(0).intensidade[1] - imagemCrescimentoRegioes.get(i, j)[1]) +
-                                        Math.abs(regioes.get(k).pixels.get(0).intensidade[2] - imagemCrescimentoRegioes.get(i, j)[2]) <= diferencaCores) {
-                            regioes.get(k).pixels.add(pixel);
-                            adicionado = true;
-                            System.out.println("Região existente");
-
-                            /*Atualiza na imagem do crescimento a nova tonalidade do pixel, de acordo com a tonalidade
-                            do primeiro pixel daquela região*/
-                            imagemCrescimentoRegioes.put(i, j, regioes.get(k).pixels.get(0).intensidade);
-                            break;
-                        }
-                    }
-
-                    //Se o pixel não foi adicionado a nenhuma região já existente, então ele é o primeiro de uma nova região
-                    if(!adicionado) {
-                        Regiao novaRegiao = new Regiao();
-                        novaRegiao.pixels.add(pixel);
-                        regioes.add(novaRegiao);
-                        System.out.println("Nova região");
+            //Crescimento de regiões a partir do pixel central.
+            Mat imagemCrescimentoRegioes = imagem.clone();
+            regiao.pixels.add(new Pixel(xSeed, ySeed, imagemCrescimentoRegioes.get(xSeed, ySeed)));
+            byte[] srcData = new byte[(int) (imagemCrescimentoRegioes.total() * imagemCrescimentoRegioes.channels())]; //Dados dos pixels da imagem
+            imagemCrescimentoRegioes.get(0, 0, srcData); //Pegando os dados em termos de coordenadas de linhas e colunas
+            for (int i = 1; i < imagemCrescimentoRegioes.rows(); i++) {
+                for (int j = 1; j < imagemCrescimentoRegioes.cols(); j++) {
+                    Pixel pixel = new Pixel(i, j, imagemCrescimentoRegioes.get(i, j));
+                    if( Math.abs(regiao.pixels.get(0).intensidade[0] - imagemCrescimentoRegioes.get(i, j)[0]) +
+                            Math.abs(regiao.pixels.get(0).intensidade[1] - imagemCrescimentoRegioes.get(i, j)[1]) +
+                            Math.abs(regiao.pixels.get(0).intensidade[2] - imagemCrescimentoRegioes.get(i, j)[2]) <= limiar) {
+                        regiao.pixels.add(pixel);
+                        imagemCrescimentoRegioes.put(i, j, regiao.pixels.get(0).intensidade);
+                    } else {
+                        imagemCrescimentoRegioes.put(i, j, corRGB);
                     }
                 }
             }
+
+            //Salva o resultado em um arquivo
+            Image image = HighGui.toBufferedImage(imagemCrescimentoRegioes);
+            BufferedImage bi = (BufferedImage) image;
+            saveImage(diretorio.getPath()+"/crescimento/"+arquivos[k], bi);
         }
 
-        HighGui.imshow("Crescimento de regiões", imagemCrescimentoRegioes);
-
-        HighGui.waitKey();
         System.exit(0);
     }
 
@@ -95,12 +71,33 @@ public class RegionGrowing {
         int linha;
         int coluna;
         double[] intensidade;
+        int[][] vizinhos = new int[8][2];
 
         public Pixel(int l, int c, double[] intensidade) {
             this.linha = l;
             this.coluna = c;
             this.intensidade = intensidade;
+
+            vizinhos[0] = new int[] {l+1, c};
+            vizinhos[1] = new int[] {l, c+1};
+            vizinhos[2] = new int[] {l-1, c};
+            vizinhos[3] = new int[] {l, c-1};
+            vizinhos[4] = new int[] {l+1, c+1};
+            vizinhos[5] = new int[] {l+1, c-1};
+            vizinhos[6] = new int[] {l-1, c+1};
+            vizinhos[7] = new int[] {l-1, c-1};
         }
     }
 
+    public static void saveImage(String filename,
+                                 BufferedImage image) {
+        File file = new File(filename);
+
+        try {
+            ImageIO.write(image, "png", file);
+        } catch (Exception e) {
+            System.out.println(e.toString()+" Imagem '"+filename
+                    +"' falhou ao salvar.");
+        }
+    }
 }
